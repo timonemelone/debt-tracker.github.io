fb734240225cffd599a6e46014fbeb1de9bc4ed8
timonemelone <128521512+timonemelone@users.noreply.github.com>
2025-07-02 10:44:36 +0200

    fix

--- src/db.js
+++ src/db.js
@@ -1,9 +1,15 @@
+const fs     = require('fs');
 const sqlite3 = require('sqlite3').verbose();
 const path    = require('path');
 const bcrypt  = require('bcrypt');
 
+const dataDir = path.join(__dirname, '..', 'data');
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+  console.log('✔️  data-Verzeichnis angelegt');
+
 // Pfad zur Datenbank-Datei (unter /data)
-const dbPath = path.join(__dirname, '..', 'data', 'db.sqlite');
+const dbPath = path.join(dataDir, 'db.sqlite');
 const db     = new sqlite3.Database(dbPath);
 
 // Tabellen anlegen, falls sie noch nicht existieren
